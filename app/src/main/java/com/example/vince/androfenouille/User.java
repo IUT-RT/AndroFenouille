7898c2574bfeb5dfeb6c49649962a5a6cd6302f6
leirof <vince.lrf@gmail.com>
2017-03-07 16:51:28 +0100

    Class User: Ajout es méthodes de recherche & passage variables en angais Ajout de: ArrayList<User> getUsersById(String) ArrayList<User> getUsersByName(String) ArrayList<User> getUsersByFirstName(String) ArrayList<User> getUsersByFormation(String) ArrayList<User> getUsersByYear(String)

--- app/src/main/java/com/example/vince/androfenouille/User.java
+++ app/src/main/java/com/example/vince/androfenouille/User.java
@@ -8,66 +8,112 @@ import java.util.ArrayList;
 public class User {
     private static ArrayList<User> userList = new ArrayList<User>();
     private static String vJeton;
-    private int idEtu;
-    private String nom;
-    private String prenom;
+    private int id;
+    private String name;
+    private String firstName;
     private String formation;
-    private int annee;
+    private int year;
 
-    public User(int idEtu, String nom, String prenom, String formation, int annee){
-        this.idEtu = idEtu;
-        this.nom = nom;
-        this.prenom = prenom;
+    public User(int id, String name, String firstName, String formation, int year){
+        this.id = id;
+        this.name = name;
+        this.firstName = firstName;
         this.formation = formation;
-        this.annee = annee;
+        this.year = year;
         userList.add(this);
     }
 
     //Lecture
 
-    public int getIdEtu(){
-        return idEtu;
+    public int getId(){
+        return id;
     }
-    public String getNom(){
-        return nom;
+    public String getName(){
+        return name;
     }
-    public String getPrenom(){
-        return prenom;
+    public String getFirstName(){
+        return firstName;
     }
     public String getFormation(){
         return formation;
     }
-    public int getAnnee(){
-        return annee;
+    public int getYear(){
+        return year;
     }
     public ArrayList<User> getUserList(){
         return userList;
     }
 
     public String getLinkToAdd(){
-        return "http://infort.gautero.fr/add.php?jeton=" + vJeton + "&nom=" + nom + "&prenom=" + prenom + "&formation=" + formation + "&annee=" + Integer.toString(annee);
+        return "http://infort.gautero.fr/add.php?jeton=" + vJeton + "&nom=" + name + "&prenom=" + firstName + "&formation=" + formation + "&annee=" + Integer.toString(year);
     }
 
     public String toString(){
-        return Integer.toString(idEtu) + "|" + nom + "|" + prenom + "|" + formation + "|" + Integer.toString(annee);
+        return Integer.toString(id) + "|" + name + "|" + firstName + "|" + formation + "|" + Integer.toString(year);
+    }
+
+    public ArrayList<User> getUsersById(String id){
+        ArrayList<User> resultList = new ArrayList<User>();
+        for(int i = 0; i < userList.size(); i +=1){
+            if(Integer.toString(userList.get(i).getId()) == id){
+                resultList.add(userList.get(i));
+            }
+        }
+        return resultList;
+    }
+    public ArrayList<User> getUsersByName(String name){
+        ArrayList<User> resultList = new ArrayList<User>();
+        for(int i = 0; i < userList.size(); i +=1){
+            if(userList.get(i).getName() == name){
+                resultList.add(userList.get(i));
+            }
+        }
+        return resultList;
+    }
+    public ArrayList<User> getUsersByFirstName(String firstName){
+        ArrayList<User> resultList = new ArrayList<User>();
+        for(int i = 0; i < userList.size(); i +=1){
+            if(userList.get(i).getFirstName() == firstName){
+                resultList.add(userList.get(i));
+            }
+        }
+        return resultList;
+    }
+    public ArrayList<User> getUsersByFormation(String formation){
+        ArrayList<User> resultList = new ArrayList<User>();
+        for(int i = 0; i < userList.size(); i +=1){
+            if(userList.get(i).getFormation() == formation){
+                resultList.add(userList.get(i));
+            }
+        }
+        return resultList;
+    }
+    public ArrayList<User> getUsersByYear(String year){
+        ArrayList<User> resultList = new ArrayList<User>();
+        for(int i = 0; i < userList.size(); i +=1){
+            if(Integer.toString(userList.get(i).getYear()) == year){
+                resultList.add(userList.get(i));
+            }
+        }
+        return resultList;
     }
 
     //Ecriture
 
-    public void setIdEtu(int idEtu){
-        this.idEtu = idEtu;
+    public void setId(int id){
+        this.id = id;
     }
-    public void setNom(String nom){
-        this.nom = nom;
+    public void setNom(String name){
+        this.name = name;
     }
-    public void setPrenom(String prenom){
-        this.prenom = prenom;
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
     }
     public void setFormation(String formation){
         this.formation = formation;
     }
-    public void setAnnee(int annee){
-        this.annee = annee;
+    public void setYear(int year){
+        this.year = year;
     }
     public void setUserList(ArrayList<User> userList){
         this.userList = userList;
